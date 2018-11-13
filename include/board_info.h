1e74a332d2734c2ba87b31c623c5801c3afc3d5b
Alessandro Latif <alatif@e1z1r5p7.42.us.org>
2018-11-12 16:22:13 -0800

    changes to header errors

--- include/board_info.h
+++ include/board_info.h
@@ -2,9 +2,6 @@
 #ifndef BOARD_INFO_H
 # define BOARD_INFO_H
 
-# define ERR_MAP ("No valid map in file \"%s\"\n")
-# define ERR_MALLOC ("Out of memory! malloc() failed...\n")
-
 typedef struct		s_board
 {
 	unsigned int	lines;
