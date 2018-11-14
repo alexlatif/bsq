0f65e326538fa11ed7858015fea12832b54e7d39
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-11-13 23:37:10 -0800

    ft_putnbr upgrade

--- lib/ft/src/ft_puts.c
+++ lib/ft/src/ft_puts.c
@@ -20,26 +20,19 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	ft_putnbr(int nb)
+void	ft_putnbr(int nbr)
 {
-	if (nb == -2147483648)
+	long	n;
+
+	n = nbr;
+	if (nbr < 0)
 	{
 		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483648);
+		n *= -1;
 	}
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(-nb);
-	}
-	else if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-	{
-		ft_putchar(nb + '0');
-	}
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+
+	ft_putchar(n % 10 + '0');
 }
