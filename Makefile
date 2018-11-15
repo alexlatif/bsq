08935911aaa687d029a8cc8c855be309683b5bb3
Alessandro Latif <alatif@e1z1r5p7.42.us.org>
2018-11-14 19:46:00 -0800

    bsq final v1

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ LDIR	= lib/ft
 CC	= gcc
 CFLAGS	+= -I $(IDIR) -I $(IDIR_MY)
 CFLAGS	+= -Wall -Wextra
-# CFLAGS	+= -Werror
+CFLAGS	+= -Werror
 
 SRCS_DIR		= src/
 SRCS_FILES		= get_board_matrix.c	\
@@ -27,18 +27,18 @@ RM	= rm -f
 all: $(LIB) $(NAME)
 
 $(LIB):
-	make -C $(LDIR)
+	@make -C $(LDIR)
 
 $(NAME): $(OBJS)
-	$(CC) -o $(NAME) $(OBJS)  -L $(LDIR) -l $(LNAME)
+	@$(CC) -o $(NAME) $(OBJS)  -L $(LDIR) -l $(LNAME)
 
 clean:
-	$(RM) $(OBJS)
-	make -C $(LDIR) clean
+	@$(RM) $(OBJS)
+	@make -C $(LDIR) clean
 
 fclean: clean
-	$(RM) $(NAME)
-	make -C $(LDIR) fclean
+	@$(RM) $(NAME)
+	@make -C $(LDIR) fclean
 
 re: fclean all
 
--- src/get_board_matrix.c
+++ src/get_board_matrix.c
@@ -30,6 +30,23 @@ void	print_board(char **board)
 	}
 }
 
+int		create_fline_list(t_list **fline, int fd)
+{
+	int		width;
+	char	c;
+
+	width = 0;
+	while (read(fd, &c, 1) > 0 && c != '\n')
+	{
+		if (!width)
+			*fline = ft_create_elem(c);
+		else
+			ft_list_push_back(fline, c);
+		width++;
+	}
+	return (width);
+}
+
 char	*get_arr_from_link(char *str, t_list *fline, t_board binfo)
 {
 	int		i;
@@ -46,6 +63,24 @@ char	*get_arr_from_link(char *str, t_list *fline, t_board binfo)
 	return (str);
 }
 
+char	*get_matrix_arr(char *str, t_board binfo, int fd, int width)
+{
+	int		j;
+	char	c;
+
+	j = 0;
+	while (read(fd, &c, 1) > 0 && c != '\n')
+	{
+		if (!check_valid_char(c, binfo))
+			ft_exit(ERR_VAL_BOARD);
+		str[j++] = c;
+	}
+	if (j != width)
+		ft_exit(ERR_VAL_BOARD);
+	str[j] = '\0';
+	return (str);
+}
+
 char	**get_board_matrix(int fd, t_board binfo)
 {
 	char		**matrix;
@@ -53,37 +88,17 @@ char	**get_board_matrix(int fd, t_board binfo)
 	int			width;
 	char		c;
 	int			i;
-	int			j;
 
-	width = 0;
-	matrix = malloc(sizeof(char*) * binfo.lines);
-	while (read(fd, &c, 1) > 0 && c != '\n')
-	{
-		if (!width)
-			fline = ft_create_elem(c);
-		else
-			ft_list_push_back(&fline, c);
-		width++;
-	}
 	i = 0;
+	width = create_fline_list(&fline, fd);
+	matrix = malloc(sizeof(char*) * binfo.lines + 1);
 	while (i < binfo.lines)
 	{
 		matrix[i] = malloc(sizeof(char) * width + 1);
 		if (i == 0)
 			matrix[i] = get_arr_from_link(matrix[i], fline, binfo);
 		else
-		{
-			j = 0;
-			while (read(fd, &c, 1) > 0 && c != '\n')
-			{
-				if (!check_valid_char(c, binfo))
-					ft_exit(ERR_VAL_BOARD);
-				matrix[i][j++] = c;
-			}
-			if (j != width)
-				ft_exit(ERR_VAL_BOARD);
-			matrix[i][j] = '\0';
-		}
+			matrix[i] = get_matrix_arr(matrix[i], binfo, fd, width);
 		i++;
 	}
 	if (read(fd, &c, 1))
